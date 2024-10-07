f0cb7de25484621217abde9948878a739db2d6d0
ahmedmohamed26 <ahmedmohamed26>
2024-10-08 00:28:24 +0300

    Add header (UI)

---
+++ app/[locale]/study-research/page.jsx
@@ -0,0 +1,3 @@
+export default function StudyAndResearch() {
+  return <div>study And Research</div>;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+      include: /assets/,
+    });
+
+    return config;
+  },
+  // reactStrictMode: false,
 };
 
 export default withNextIntl(nextConfig);
